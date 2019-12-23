b74bd8a865ccde82be0f8e77e6ea6e5316f9a722
frojo <mojoz0101@gmail.com>
2019-12-23 16:05:22 -0500

    first commit?

--- src/index.js
+++ src/index.js
@@ -1,12 +1,63 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function formatName(user) {
+  return user.first_name + ' ' + user.last_name
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const me = {
+  first_name: 'fran',
+  last_name: 'rojo'
+}
+const name = 'francisco';
+var portfolio_container = <div></div>;
+
+// function Heading(props) {
+//   return <h1>my name is {props.name}</h1>;
+// }
+
+function Project(props) {
+  return <div>this project is {props.name}!</div>;
+}
+
+function ProjectPage(props) {
+  return <div>this project is {props.name}!</div>;
+}
+
+class ProjectThumbnail extends React.Component {
+  render () {
+    return (
+      <a href="/poop/poop">
+	<img 
+      	  src={this.props.url}
+      	  alt={this.props.title}
+      	/>
+      </a>
+    )
+  }
+}
+
+
+function App(props) {
+  return (
+    <div>
+      <h1>francisco rojo</h1>
+      <div>
+	<ProjectThumbnail url='poop' title='what do you wish you could say?' />
+	<ProjectThumbnail url='poop' title='a summer afternoon in bloomfield' />
+      </div>
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+
+
+
+
+
+
+
