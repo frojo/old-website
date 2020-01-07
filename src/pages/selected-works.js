712dc4effbfeced2b24cc4331d4389b76a7110d3
frojo <mojoz0101@gmail.com>
2020-01-07 16:47:50 -0500

    new favicon, and new image

--- src/pages/selected-works.js
+++ src/pages/selected-works.js
@@ -3,25 +3,24 @@ import React from 'react';
 // todo: generate this dynamically or whatever
 const projects = [
   {
-    name: 'what do you wish you could say',
+    name: 'what do you wish you could say?',
     image_path: '/images/wdywycs-cover.jpg'
   },
   {
     name: 'a summer afternoon in bloomfield',
-    image_path: 'poop'
+    image_path: '/images/bloomfield-cover.png'
   }
 ];
 
 function ProjectThumbnail(props) {
-  return <img className='project-thumbnail' src={props.image_path} />;
+  return <img className='project-thumb' src={props.image_path} alt={props.alt}/>;
 }
 
 function SelectedWorks(props) {
   // const projects = props.projects
   const list_items = projects.map((project) =>
     <li>
-      <ProjectThumbnail image_path={project.image_path} title={project.name} />
-      <b>{project.name}</b>
+      <ProjectThumbnail image_path={project.image_path} alt={project.name} />
     </li>
   );
   return <ul>{list_items}</ul>;
