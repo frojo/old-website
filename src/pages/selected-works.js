65c53fcb4c747426392384e8b293e0c83e7445b9
frojo <mojoz0101@gmail.com>
2020-01-08 11:58:30 -0500

    can click on photos to go to project pages

--- src/pages/selected-works.js
+++ src/pages/selected-works.js
@@ -1,29 +1,37 @@
-import React from 'react';
+import React from 'react'
+import { Link } from 'react-router-dom'
 
 // todo: generate this dynamically or whatever
 const projects = [
   {
+    id: 'wdywycs',
     name: 'what do you wish you could say?',
     image_path: '/images/wdywycs-cover.jpg'
   },
   {
+    id: 'bloomfield',
     name: 'a summer afternoon in bloomfield',
     image_path: '/images/bloomfield-cover.png'
   }
-];
+]
 
 function ProjectThumb(props) {
-  return <img className='project-thumb' src={props.image_path} alt={props.alt}/>;
+  return <img className='project-thumb' 
+	  src={props.image_path} 
+	  alt={props.alt}
+	  />
 }
 
 function ProjectList(props) {
   // const projects = props.projects
   const list_items = projects.map((project) =>
     <li>
-      <ProjectThumb image_path={project.image_path} alt={project.name} />
+      <Link to={'/work/' + project.id}>
+	<ProjectThumb image_path={project.image_path} alt={project.name} />
+      </Link>
     </li>
-  );
-  return <ul className='project-thumb-container'>{list_items}</ul>;
+  )
+  return <ul className='project-thumb-container'>{list_items}</ul>
 
 }
 
@@ -33,7 +41,7 @@ function SelectedWorks(props) {
       <h2>selected works</h2>
       <ProjectList />
     </div>
-  );
+  )
 }
-export default SelectedWorks;
+export default SelectedWorks
 
