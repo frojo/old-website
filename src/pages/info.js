9232c914f0d2dbfee97ec11ccd8a8f535479700f
frojo <mojoz0101@gmail.com>
2020-02-15 14:34:39 -0500

    some work on the about/info page

--- src/pages/info.js
+++ src/pages/info.js
@@ -4,8 +4,44 @@ import React from 'react';
 function InfoPage(props) {
   return (
     <div>
-      i am fran this is my info page
+      <h3>About me</h3>
 
+      I live in Brooklyn.
+
+
+      <h3>About my work</h3>
+      <i> In this section, I 
+
+
+      <div>
+	Please select the option that most precisely describes you: 
+	<select>
+	  <option> ~ default ~ </option>
+	  <option>I went to art school. Hit me with that Artist Statment word porn.</option>
+	  <option>I'm into wierd exprimental vidya games </option>
+	  <option>Stop hiding behind this cynical front and tell me what you do</option>
+	  <option>wait, these are all the same</option>
+	</select>
+      </div>
+
+      I make work that is <i>interactive</i>. In other words, 
+      I design and create spaces for people to interact in
+
+      I make games, insofar as my work defines spaces and rules to exist within.
+      When I make something, I make something that is interactive, that defines
+      a space and a set of rules, that is sometimes playful, sometimes delightful.
+      But i don't worry about making things that are <i>fun</i>
+
+
+      Games are interesting because they define rules and space that people play within
+
+
+    , you can usually materially affect it. Often, my work is personal. 
+
+      I love videogames, and for a while I was sure I wanted to make games. But I also hate videogames. Some of my work tends to look like a game, but it feels weirdI rarely make things that are f
+
+
+      I'm currently extra-interested in teaching, speaking, or having my work exhibited. Contact me at franmorojo at gmail dot com.
 
       // broad strokes/manifesto
       <i> herein, i use intellectual language to describe how i live my life, 
